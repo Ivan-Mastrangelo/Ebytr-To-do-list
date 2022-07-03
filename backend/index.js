c3efedb3680f0f6fc6b52ccf3f5e2fc93dc58966
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-02 21:29:40 -0300

    feat: criar as rotas para tasks e inseri-las no index

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const taskRoutes = require('./src/routes/TasksRoutes');
+const errorMiddleware = require('./src/middlewares/ErrorMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use('/tasks', taskRoutes);
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Running on port ${process.env.PORT}`);
---
+++ backend/src/routes/TasksRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const TaskController = require('../controllers/TaskController');
+
+router.post('/', TaskController.creatTask);
+router.get('/', TaskController.getAllTasks);
+router.get('/:id', TaskController.getTaskById);
+router.put('/:id', TaskController.updateTask);
+router.delete('/:id', TaskController.deleteTask);
+
+module.exports = router;
